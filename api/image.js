6d8f2b2ac7a8c4d63aed00d8af54a8a9adcf5a41
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-11-20 12:41:25 -0300

    update api/image.js

--- api/image.js
+++ api/image.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('node:path');
 const { createCanvas, loadImage, GlobalFonts } = require('@napi-rs/canvas');
 const { getUserInfo } = require('../helpers/disav');
-GlobalFonts.registerFromPath(join(__dirname, '..', 'fonts', 'arial.ttf'), 'Arial');
+GlobalFonts.registerFromPath(path.join(__dirname, '..', 'fonts', 'arial.ttf'), 'Arial');
 
 const app = express();
 

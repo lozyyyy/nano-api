b01801e103c9b41e4f56191d21580387afde85d3
Lucas <111234099+Lucas46521@users.noreply.github.com>
2024-11-21 12:05:14 -0300

    update api/image.js

--- api/image.js
+++ api/image.js
@@ -272,12 +272,12 @@ app.get('/api/rank', async (req, res) => {
 
     const positions = [
   { x: 85, y: 220 },  // 1º item
-  { x: 85, y: 275 },  // 2º item
+  { x: 85, y: 300 },  // 2º item
   { x: 85, y: 340 },  // 3º item
   { x: 85, y: 405 },  // 4º item
   { x: 85, y: 470 },  // 5º item
   { x: 445, y: 220 }, // 6º item
-  { x: 445, y: 275 }, // 7º item
+  { x: 445, y: 300 }, // 7º item
   { x: 445, y: 340 }, // 8º item
   { x: 445, y: 405 }, // 9º item
   { x: 445, y: 470 }, // 10º item
